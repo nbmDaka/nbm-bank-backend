b3fb173799a4611bdb35b9662aab2d097a71dbdb
nbmdaka <nurdaulet.beketov.2005@gmail.com>
2026-09-05 18:13:46 +0500

    feat: add CreateUser method for user creation and password hashing

--- services/user-service/go.mod
+++ services/user-service/go.mod
@@ -1,6 +1,6 @@
 module github.com/nbmDaka/nbm-bank-backend/services/user-service
 
-go 1.25.0
+go 1.26.0
 
 require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -8,6 +8,7 @@ require (
 	github.com/jackc/pgx/v5 v5.10.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	golang.org/x/crypto v0.56.0 // indirect
 	golang.org/x/net v0.58.0 // indirect
 	golang.org/x/sync v0.22.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
--- services/user-service/internal/user/application/service.go
+++ services/user-service/internal/user/application/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/nbmDaka/nbm-bank-backend/services/user-service/internal/user/domain"
+	"github.com/nbmDaka/nbm-bank-backend/services/user-service/internal/user/password"
 )
 
 type UserService struct {
@@ -27,5 +28,32 @@ func (s *UserService) GetUser(
 	return s.repo.GetByID(
 		ctx,
 		id,
+	)
+}
+
+
+func (s *UserService) CreateUser(
+	ctx context.Context,
+	user *domain.User,
+	rawPassword string,
+) error {
+
+
+	hash, err := password.Hash(
+		rawPassword,
+	)
+
+
+	if err != nil {
+		return err
+	}
+
+
+	user.PasswordHash = hash
+
+
+	return s.repo.Create(
+		ctx,
+		user,
 	)
 }
